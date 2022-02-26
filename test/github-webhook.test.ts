8f5a831005cd15e0584d1fb5c95de25594aa40be
EC2 Default User <ec2-user@ip-172-31-27-93.ca-central-1.compute.internal>
2022-02-26 16:22:57 +0000

    test(cdk): add test to ensure lambda is created with correct handler

--- test/github-webhook.test.ts
+++ test/github-webhook.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as GithubWebhook from '../lib/github-webhook-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as GithubWebhook from '../lib/github-webhook-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/github-webhook-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
+test('GithubWebhook Lambda Created', () => {
+   const app = new cdk.App();
 //     // WHEN
-//   const stack = new GithubWebhook.GithubWebhookStack(app, 'MyTestStack');
+   const stack = new GithubWebhook.GithubWebhookStack(app, 'TestWebhookStack');
 //     // THEN
-//   const template = Template.fromStack(stack);
+   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+   template.hasResourceProperties('AWS::Lambda::Function', {
+     Handler: 'webhook.handler'
+   });
 });
