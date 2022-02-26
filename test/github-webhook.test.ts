5d7f0e02c5372e90b2c39a6015bf1bfd35583db9
EC2 Default User <ec2-user@ip-172-31-27-93.ca-central-1.compute.internal>
2022-02-26 13:35:13 +0000

    Initial commit

---
+++ test/github-webhook.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GithubWebhook from '../lib/github-webhook-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/github-webhook-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GithubWebhook.GithubWebhookStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
